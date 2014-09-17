3263de9354c56d52578dc344e7556588c038c6e9
n3yang <n3yang@gmai.com>
2014-09-17 20:28:07 +0800

    update

--- content/themes/gugong/archive-product.php
+++ content/themes/gugong/archive-product.php
@@ -4,9 +4,8 @@
  *
  * Override this template by copying it to yourtheme/woocommerce/archive-product.php
  *
- * @author 		WooThemes
- * @package 	WooCommerce/Templates
- * @version     2.0.0
+ * @author 		n3yang
+ * @package 	GugongShop
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
@@ -21,71 +20,33 @@ get_header( 'shop' ); ?>
 		 * @hooked woocommerce_breadcrumb - 20
 		 */
 		do_action( 'woocommerce_before_main_content' );
-	?>
 
-		<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
 
-			<h1 class="page-title"><?php woocommerce_page_title(); ?></h1>
-
-		<?php endif; ?>
-
-		<?php do_action( 'woocommerce_archive_description' ); ?>
-
-		<?php if ( have_posts() ) : ?>
-
-			<?php
-				/**
-				 * woocommerce_before_shop_loop hook
-				 *
-				 * @hooked woocommerce_result_count - 20
-				 * @hooked woocommerce_catalog_ordering - 30
-				 */
+		if (is_tax()){
+			if ( have_posts() ) {
 				do_action( 'woocommerce_before_shop_loop' );
-			?>
-
-			<?php woocommerce_product_loop_start(); ?>
-
-				<?php woocommerce_product_subcategories(); ?>
-
-				<?php while ( have_posts() ) : the_post(); ?>
-
-					<?php wc_get_template_part( 'content', 'product' ); ?>
-
-				<?php endwhile; // end of the loop. ?>
-
-			<?php woocommerce_product_loop_end(); ?>
-
-			<?php
-				/**
-				 * woocommerce_after_shop_loop hook
-				 *
-				 * @hooked woocommerce_pagination - 10
-				 */
+				woocommerce_product_loop_start();
+				woocommerce_product_subcategories();
+				while ( have_posts() ) : the_post(); 
+					 wc_get_template_part( 'content', 'product' );
+				endwhile; // end of the loop.
+				woocommerce_product_loop_end();
 				do_action( 'woocommerce_after_shop_loop' );
-			?>
+			} else {
+				wc_get_template( 'loop/no-products-found.php' );
+			}
+			$term = get_queried_object();
+			
+		} else {
+			get_template_part('content', 'shop');
+		}
 
-		<?php elseif ( ! woocommerce_product_subcategories( array( 'before' => woocommerce_product_loop_start( false ), 'after' => woocommerce_product_loop_end( false ) ) ) ) : ?>
-
-			<?php wc_get_template( 'loop/no-products-found.php' ); ?>
-
-		<?php endif; ?>
-
-	<?php
 		/**
 		 * woocommerce_after_main_content hook
 		 *
 		 * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
 		 */
 		do_action( 'woocommerce_after_main_content' );
-	?>
-
-	<?php
-		/**
-		 * woocommerce_sidebar hook
-		 *
-		 * @hooked woocommerce_get_sidebar - 10
-		 */
-		do_action( 'woocommerce_sidebar' );
 	?>
 
 <?php get_footer( 'shop' ); ?>
---
+++ content/themes/gugong/content-shop.php
@@ -0,0 +1,162 @@
+
+	<div class="main">
+		<div class="banner">
+			<div id="ui-slide">
+		        <ul class="slide">
+		            <li class="slide-panel" data-pic="<?php bloginfo('template_url'); ?>/images/11.jpg">
+		                <div class="slide-pic">
+		                	<a style="position:absolute;width:1200px;height:367px;" href="category/toys"></a>
+		                </div>
+		            </li>
+		            <li class="slide-panel" data-pic="<?php bloginfo('template_url'); ?>/images/22.jpg">
+		              <div class="slide-pic"></div>
+		            </li>
+		            <li class="slide-panel" data-pic="<?php bloginfo('template_url'); ?>/images/33.jpg">
+		                <div class="slide-pic"></div>
+		            </li>
+		            <li class="slide-panel" data-pic="<?php bloginfo('template_url'); ?>/images/44.jpg">
+		                <div class="slide-pic"></div>
+		            </li>
+		             <li class="slide-panel" data-pic="<?php bloginfo('template_url'); ?>/images/55.jpg">
+		                <div class="slide-pic"></div>
+		            </li>
+		             <li class="slide-panel" data-pic="<?php bloginfo('template_url'); ?>/images/66.jpg">
+		                <div class="slide-pic"></div>
+		            </li>
+		             <li class="slide-panel" data-pic="<?php bloginfo('template_url'); ?>/images/77.jpg">
+		                <div class="slide-pic"></div>
+		            </li>
+		             <li class="slide-panel" data-pic="<?php bloginfo('template_url'); ?>/images/88.jpg">
+		                <div class="slide-pic"></div>
+		            </li>
+		        </ul>
+		        <div class="slide-trigger dib-wrap">
+		            <a class="dib a1 active"></a>
+		            <a class="dib a2"></a>
+		            <a class="dib a3"></a>
+		            <a class="dib a4"></a>
+		            <a class="dib a5"></a>
+		            <a class="dib a6"></a>
+		            <a class="dib a7"></a>
+		            <a class="dib a8"></a>
+		        </div>
+
+		        <div class="slide-btn">
+		        	<a class="prev" href="javascript:;"></a>
+		        	<a class="next" href="javascript:;"></a>
+		        </div>
+		    </div>
+		</div>
+	</div>
+
+<script>
+(function($){
+		
+	//slide
+	$.fn.slide = function(options){
+		//设置默认参数
+		var defaults = {    
+  			aLi: this.find('.slide-panel'),
+			aA: this.find('.slide-trigger a'),
+			aBtn: this.find('.slide-btn a'),
+			autoPlayTime: 3000,
+			showTime: 1000,
+			setInit: true
+		};
+		var play = null,
+			page = 0;
+		var opts = $.extend(defaults, options);
+		//初始化slide
+		if(opts.setInit) init();
+	
+		//初始化slide
+		//设置宽slide li背景
+		//设置内slide div.slide-pic背景
+		function init(){
+			opts.aLi.each(function(){
+				$(this).css({
+					'opacity': 0,
+					'z-index': 0
+				});
+				$(this).find('.slide-pic').css('background-image', 'url('+ $(this).attr('data-pic') +')');
+			});
+			opts.aLi.eq(0).css({
+				"opacity": 1,
+				'z-index': 1
+			}); 
+		};
+		
+		function showSlide(){
+			opts.aA.eq( page ).addClass( "active" ).siblings().removeClass( "active" );
+			opts.aLi.eq( page ).css({ "opacity" : 1 });
+			opts.aLi.eq( page ).siblings().animate( { "opacity" : 0 }, opts.showTime, function(){
+				opts.aLi.eq( page ).css({"z-index":1}).siblings().css({"z-index":0});
+			});
+		};
+		
+		function playSlide(){
+			if( !opts.aLi.is( ":animated" )){
+				if( page == opts.aLi.length - 1 ){
+					page = 0;
+					showSlide();
+				}else{
+					page++;
+					showSlide();
+				}
+			}
+		};
+		
+		opts.aBtn.eq(1).click(function () {
+			if( !opts.aLi.is( ":animated" )){
+				if( page == opts.aLi.length - 1 ){
+					page = 0;
+					showSlide();
+				}else{
+					page++;
+					showSlide();
+				}
+			}
+		});
+
+		opts.aBtn.eq(0).click(function () {
+			if( !opts.aLi.is( ":animated" )){
+				if( page == 0){
+					page = opts.aLi.length - 1;
+					showSlide();
+				}else{
+					page--;
+					showSlide();
+				}
+			}
+		});
+
+		opts.aA.click(function(){
+			if( !opts.aLi.is( ":animated" ) ){
+				var index = opts.aA.index( this ); 
+				page = index;
+				showSlide();
+			}
+		});
+		
+		
+		this.hover( function(){
+			clearInterval(play);
+		}, function(){
+			play = setInterval(function(){
+				playSlide();
+			}, opts.autoPlayTime);
+		}).trigger( "mouseleave" );
+	};
+	
+	
+})(jQuery);
+
+
+$(document).ready(function() {
+	$('#ui-slide').slide({
+		autoPlayTime: 10000,
+		showTime: 1000
+	});
+
+});
+</script>
