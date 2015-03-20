79b24233f1ee523575556e1df4829439e1ef6d4f
n3yang <n3yang@gmai.com>
2015-03-21 00:38:06 +0800

    update: alipay

--- content/plugins/alipay-for-woocommerce/class-wc-alipay.php
+++ content/plugins/alipay-for-woocommerce/class-wc-alipay.php
@@ -490,23 +490,23 @@ class WC_Alipay extends WC_Payment_Gateway {
 
         wc_enqueue_js( '
             $.blockUI({
-                    message: "' . esc_js( __( 'Thank you for your order. We are now redirecting you to Alipay to make payment.', 'alipay' ) ) . '",
-                    baseZ: 99999,
-                    overlayCSS:
-                    {
-                        background: "#fff",
-                        opacity: 0.6
-                    },
-                    css: {
-                        padding:        "20px",
-                        zindex:         "9999999",
-                        textAlign:      "center",
-                        color:          "#555",
-                        border:         "3px solid #aaa",
-                        backgroundColor:"#fff",
-                        cursor:         "wait",
-                        lineHeight:     "24px",
-                    }
+                    message: "' . esc_js( __( 'Thank you for your order. We are now redirecting you to Alipay to make payment.', 'alipay' ) ) . '"
+                    // baseZ: 99999,
+                    // overlayCSS:
+                    // {
+                    //     background: "#fff",
+                    //     opacity: 0.6
+                    // },
+                    // css: {
+                    //     padding:        "20px",
+                    //     zindex:         "9999999",
+                    //     textAlign:      "center",
+                    //     color:          "#555",
+                    //     border:         "3px solid #aaa",
+                    //     backgroundColor:"#fff",
+                    //     cursor:         "wait",
+                    //     lineHeight:     "24px",
+                    // }
                 });
             jQuery("#submit_alipay_payment_form").click();
         ' );
