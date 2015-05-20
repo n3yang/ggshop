6a98ede6c1e72f74b9ea47fe8f0768ed25244bff
n3yang <n3yang@gmai.com>
2015-05-20 17:49:59 +0800

    update: wechatpay

--- content/plugins/wechatpay-for-woocommerce/wc-class-wechatpay.php
+++ content/plugins/wechatpay-for-woocommerce/wc-class-wechatpay.php
@@ -5,10 +5,11 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
  * Wechatpay Payment Gateway
  *
  * Wechatpay Payment Gateway for Woocommerce
+ * 采用微信原生支付的模式2：http://pay.weixin.qq.com/wiki/doc/api/native.php?chapter=6_5
  *
- * @class 		WC_Wechatpay
- * @extends		WC_Payment_Gateway
- * @version		1.0
+ * @class         WC_Wechatpay
+ * @extends       WC_Payment_Gateway
+ * @version       1.0
  */
 
 class WC_Wechatpay extends WC_Payment_Gateway {
@@ -233,29 +234,66 @@ class WC_Wechatpay extends WC_Payment_Gateway {
      * @return void
      */
     function receipt_page( $orderId ) {
+
+        // 获取订单及产品信息
+        $order = new WC_Order($orderId);
+
+        if (!$order->needs_payment()){
+            return;
+        }
+        // var_dump($order->get_item_count());
+
+        // 调用统一下单API，生成预支付交易
+        // 统一下单API URL
+        $unifiedOrderUrl = "https://api.mch.weixin.qq.com/pay/unifiedorder";
+        // 支付时显示的标题信息
+        $bodyStr = get_bloginfo('name').'订单 #'.$orderId;
+        // 详细商品描述
+        $items = $order->get_items();
+        $detailStr = '';
+        foreach ($items as $key => $item) {
+            $detailStr .= $item['name'] . ' x '.$item['qty'] . "\n";
+        }
+        // 预支付订单信息
+        $preData = array(
+            'appid'             => $this->appId,
+            'mch_id'            => $this->merchantId,
+            'nonce_str'         => $this->build_noncestr(),
+            'body'              => $bodyStr,
+            'detail'            => $detailStr,
+            'out_trade_no'      => date('YmdHis').$orderId,
+            'total_fee'         => $order->get_total() * 100,
+            'spbill_create_ip'  => $_SERVER['REMOTE_ADDR'],
+            'notify_url'        => $this->notify_url,
+            'trade_type'        => 'NATIVE',
+            'product_id'        => $orderId,
+            );
+        $preData['sign'] = $this->build_sign($preData);
+        // to xml
+        $xml = $this->array_to_xml($preData);
+        $rdata = $this->post_xml_curl($xml, $unifiedOrderUrl);
+        // TODO:校验返回数据
+        $rdata = $this->xml_to_array($rdata);
+        if ($rdata['return_code'] != 'SUCCESS') {
+            $this->log('Unifiedorder faild. return_code:'.$rdata['return_code'] . ' return_msg:'.$rdata['return_msg']);
+            echo '微信支付平台貌似出现了问题，请稍后再试';
+            return;
+        }
+        if ($rdata['result_code']) {
+
+        }
         echo '';
-        echo $this->build_qrcode_image( $orderId );
+        echo $this->build_qrcode_image( $rdata['code_url'] );
         echo '';
     }
 
     /**
      * 构建微信支付扫描的二维码
-     * @param  int     $orderId 订单号
+     * @param  int     $bizurl  支付地址
      * @return string           二维码图片的html标签
      */
-    function build_qrcode_image($orderId) {
+    function build_qrcode_image($bizurl) {
 
-        $nativeDs["appid"] = $this->appId;
-        $nativeDs['mch_id'] = $this->merchantId;
-        $nativeDs["product_id"] = $orderId;
-        $nativeDs["time_stamp"] = time();
-        $nativeDs["nonce_str"] = $this->create_noncestr();
-        // build signature string
-        $signStr = $this->build_params_str($nativeDs).'&key='.$this->merchantKey;
-        $nativeDs["sign"] = strtoupper(md5($signStr));
-        // build pay url string
-        $bizstr = $this->build_params_str($nativeDs, false);
-        echo $bizurl = "weixin://wxpay/bizpayurl?".$bizstr;
         // build QR code
         require_once 'phpqrcode.class.php';
         ob_start();
@@ -275,28 +313,49 @@ class WC_Wechatpay extends WC_Payment_Gateway {
      */
     function check_wechatpay_response() {
 
-        if (empty($_POST)) {
+        $postraw = file_get_contents("php://input");
+        if (empty($postraw)) {
             wp_die("Invalid Requirements");
         }
-
-        file_put_contents('/tmp/wxpay.log', json_encode($_POST));
-        exit;
-
-        if (!$this->verify_response($_POST)){
-            $this->log('Wechatpay response verify faild! post:'.json_encode($_POST));
-            wp_die("Invalid Requirements");
+        if ($this->debug) {
+            $this->log('Reviece response:' . $postraw, 'Info');
         }
 
-        $orderId = substr($_POST['orderId'], 14, strlen($_POST['orderId']));
-        $order = new WC_Order($orderId);
-        if( $order->status != 'completed'){
-            $order->payment_complete();
-            $order->add_order_note ('支付成功');
-            update_post_meta( $orderId, 'Wechatpay Trade No.', wc_clean( $_POST['queryId'] ) );
-            $this->log('Payment Completed! Order ID: ' . $orderId . 'date:' . json_encode($_POST), 'Info');
-            header( 'HTTP/1.1 200 OK' );
-            echo "Success";
-            exit;
+        // 验证数据信息
+        $vdata = $this->verify_response($postraw);
+        if ($vdata == false){
+            $this->log('Wechatpay response verify faild! post:'.$postraw);
+            wp_die("Invalid Requirements");
+        }
+        if ($this->debug) {
+            $this->log('Wechatpay is verified', 'Info');
+        }
+
+        // 支付成功
+        if ($vdata['return_code'] == 'SUCCESS' || $vdata['result_code'] == 'SUCCESS') {
+
+            $orderId = substr($vdata['out_trade_no'], 14, strlen($vdata['out_trade_no']));
+            $order = new WC_Order($orderId);
+
+            if( $order->status != 'completed'){
+                $order->payment_complete();
+                $order->add_order_note ('支付成功');
+                update_post_meta( $orderId, 'Wechatpay Trade No.', wc_clean( $vdata['transaction_id'] ) );
+                $this->log('Payment Completed! Order ID: ' . $orderId , 'Info');
+                // return xml message to wechatpay
+                header( 'HTTP/1.1 200 OK' );
+                $response = array(
+                    'return_code'   => 'SUCCESS',
+                    'return_msg'    => ''
+                    );
+                echo $this->array_to_xml($response);
+                exit;
+            }
+        } else {
+            $this->log('Wechatpay response faild! Return message: ' . $vdata['return_msg'] 
+                . ' Result code: ' . $vdata['return_code']
+                . ' Result message: ' . $vdata['return_msg'] );
+            wp_die('');
         }
 
     }
@@ -336,39 +395,20 @@ class WC_Wechatpay extends WC_Payment_Gateway {
 
     /**
      * 验证支付平台通知消息
-     *
+     * 验证成功返回解析后的数组，否则返回false
+     * 
      * @access public
-     * @param string $str
+     * @param string $postraw
      * @since 1.3
-     * @return string
+     * @return bool/string 验证成功返回解析后的数组，否则返回false
      */
-    function verify_response($p) {
-        // 公钥
-        if (!$this->verifyCert) {
-            $this->log('verify response...Can not find verify Cert file.');
+    function verify_response($postraw) {
+        if (empty($postraw)) {
             return false;
         }
-        $x509data = file_get_contents ( $this->verifyCert );
-        openssl_x509_read ( $x509data );
-        $certdata = openssl_x509_parse ( $x509data );
-        $fileCertId = $certdata ['serialNumber'];
-        if ($fileCertId!=$p['certId']) {
-            $this->log('verify response... cert ID faild.');
-            return false;
-        }
-        $public_key = $x509data;
-        $this->log('verify response...: cert id: '.$p['certId'], 'Info');
-
-        // 签名串
-        $signature_str = $p['signature'];
-        unset ( $p['signature'] );
-        $params_str = $this->build_params_str($p);
-        $this->log('verify response...params str:' . $params_str, 'Info');
-        $signature = base64_decode ( $signature_str );
-        $params_sha1x16 = sha1 ( $params_str, FALSE );
-        $isSuccess = openssl_verify ( $params_sha1x16, $signature,$public_key, OPENSSL_ALGO_SHA1 );
-        $this->log( $isSuccess ? 'Success: verify response...' : 'Error: verify response...', 'Info' );
-        return $isSuccess;
+        $postdata = $this->xml_to_array($postraw);
+        // build signature string
+        return $this->build_sign($postdata) == $postdata['sign'] ? $postdata : false;
     }
 
     /**
@@ -390,29 +430,34 @@ class WC_Wechatpay extends WC_Payment_Gateway {
     }
 
     /**
-     * 将数组转换为 key=value 的形式并使用 & 连接，如果passSign=true，将跳过sign参数
+     * 将数组转换为 key=value 的形式并使用 & 连接
      * 
      * @param  array $params 
      * @param  bool $passSign
      * @return string         
      */
-    function build_params_str($params, $passSign=false)
+    function build_params_str($params)
     {
         ksort($params);
         foreach ($params as $key => $value) {
-            if ($key == 'sign' && $passSign) {
-                continue;
-            }
             $params_str .= sprintf("%s=%s&", $key, $value);
         }
         return substr($params_str, 0, strlen($params_str)-1);
     }
+    function build_sign($params, $passSign=true)
+    {
+        if (key_exists('sign', $params) && $passSign) {
+            unset($params['sign']);
+        }
+        echo $str = $this->build_params_str($params).'&key='.$this->merchantKey;
+        return strtoupper(md5($str));
+    }
     /**
-     * 产生一个随机只包含字母大小写及数字的字符串
+     * 产生一个随机只包含大小字母写及数字的字符串
      * @param  integer $length 
      * @return string
      */
-    function create_noncestr( $length = 32 ) {  
+    function build_noncestr( $length = 16 ) {  
         $chars = 'abcdefghijklmnopqrstuvwxyz0123456789';
         $str = '';
         for ( $i = 0; $i < $length; $i++ )  {
@@ -420,7 +465,83 @@ class WC_Wechatpay extends WC_Payment_Gateway {
         }
         return $str;
     }
-
+    /**
+     * 以post方式提交xml到对应的接口url
+     * 
+     * @param string $xml  需要post的xml数据
+     * @param string $url  url
+     * @param bool $useCert 是否需要证书，默认不需要
+     * @param int $timeout   url执行超时时间，默认30s
+     * @throws WxPayException
+     */
+    static function post_xml_curl($xml, $url, $useCert = false, $timeout = 30)
+    {       
+        //初始化curl        
+        $ch = curl_init();
+        //设置超时
+        curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
+        
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+        //设置header
+        curl_setopt($ch, CURLOPT_HEADER, FALSE);
+        //要求结果为字符串
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    
+        if ($useCert){
+            //设置证书
+            //使用证书：cert 与 key 分别属于两个.pem文件
+            curl_setopt($ch, CURLOPT_SSLCERTTYPE,'PEM');
+            curl_setopt($ch, CURLOPT_SSLCERT, $this->signCert);
+            curl_setopt($ch, CURLOPT_SSLKEYTYPE,'PEM');
+            curl_setopt($ch, CURLOPT_SSLKEY, $this->verifyCert);
+        }
+        //post提交方式
+        curl_setopt($ch, CURLOPT_POST, TRUE);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $xml);
+        //运行curl
+        $data = curl_exec($ch);
+        //返回结果
+        if($data){
+            curl_close($ch);
+            return $data;
+        } else { 
+            $error = curl_errno($ch);
+            curl_close($ch);
+            $this->log('CURL is failure. Error number: '.$error);
+        }
+    }
+    /**
+     * 将微信平台返回的xml数据转换为数组
+     * @param  string $xml 
+     * @return array      
+     */
+    function xml_to_array($xml)
+    {
+        $xml = simplexml_load_string($xml, 'SimpleXMLElement', LIBXML_NOCDATA);
+        return json_decode(json_encode($xml), true);
+    }
+    /**
+     * 将数组转换为xml
+     * @param  array $dataset 
+     * @return           
+     */
+    function array_to_xml($dataset)
+    {
+        if (empty($dataset))
+            return '<xml></xml>';
+        $xml = '';
+        foreach ($dataset as $k => $v) {
+            if (is_numeric($v)){
+                $xml .= "<" . $k . ">" . $v . "</" . $k . ">";
+            }else{
+                $xml .= "<" . $k . "><![CDATA[" . $v . "]]></" . $k . ">";
+            }
+        }
+        $xml = '<xml>'.$xml.'</xml>';
+        return $xml;
+    }
     /**
      * Display wechatpay Trade No. in the backend.
      * 
@@ -436,3 +557,4 @@ class WC_Wechatpay extends WC_Payment_Gateway {
         }
     }
 }
+
