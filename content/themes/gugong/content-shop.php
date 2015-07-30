3b2a86fe52f8f23a306089f460af619d8189c6d4
n3yang <n3yang@gmai.com>
2015-07-30 18:26:15 +0800

    add: banner

--- content/themes/gugong/content-shop.php
+++ content/themes/gugong/content-shop.php
@@ -2,6 +2,11 @@
 		<div class="banner">
 			<div id="ui-slide">
 		        <ul class="slide">
+		            <li class="slide-panel" data-pic="<?php bloginfo('template_url'); ?>/images/banner-0730.jpg">
+		                <div class="slide-pic">
+		             		<a style="position:absolute;width:1200px;height:367px;" href="category/presales"></a>
+		                </div>
+		            </li>
 		            <li class="slide-panel" data-pic="<?php bloginfo('template_url'); ?>/images/101.jpg">
 		                <div class="slide-pic">
 		             		<a style="position:absolute;width:1200px;height:367px;" href="category/hot"></a>
